0f6e608fcdeccabced6aaedb6842ca6c8e9ab9c2
Johannes Hope Denstadli <80511413+U25B3@users.noreply.github.com>
2024-04-24 20:41:11 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hei. Johannes her.</p>
+        <p>Hei, Johannes her</p>
         <p>Kommer side snart, bare vent</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>src/App.js</code> 
+        </p>
       </header>
     </div>
   );
