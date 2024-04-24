8114b0227dcc6fc5a709a0145c10cf4c9b14a8a4
Johannes Hope Denstadli <80511413+U25B3@users.noreply.github.com>
2024-04-24 20:23:43 +0200

    front update

--- src/App.js
+++ src/App.js
@@ -5,18 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hei, Johannes her.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Snart kommer en liten side.
+        </p>
+        <p>
+          Bare vent.
+        </p>
       </header>
     </div>
   );
